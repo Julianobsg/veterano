2df965b79ee04dab2bb95cff930b986bcdf65ae5
unknown <raquel.reiscosta@gmail.com>
2015-03-29 21:41:42 -0300

    Cenário de fundo 1.

---
+++ Assets/ColiderChanceScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColiderChanceScene : MonoBehaviour {
+
+		public string levelName;
+		public void OnTriggerStay2D(Collider2D other)
+		{
+			MoveToCharacter2D moveToCharacter = other.GetComponent<MoveToCharacter2D>();
+			if (moveToCharacter)
+			{
+				if (!moveToCharacter.IsMoving)
+				{
+					Application.LoadLevel(levelName);
+				}
+			}
+		}
+	}
