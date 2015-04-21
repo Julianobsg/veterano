48649e716f84eae607f7ddee5f6f8a3dc3f0641c
Juliano Gomes <julianobsg@gmail.com>
2015-04-21 17:30:40 -0300

    barrinha de stress + menu

---
+++ Assets/Scripts/UI/HealthBarControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBarControl : MonoBehaviour {
+
+	public Image frontImage;
+
+	public void SetBarPercent(float percent)
+	{
+		frontImage.fillAmount = percent;
+	}
+}
---
+++ Assets/Scripts/UI/HealthBarExample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarExample : MonoBehaviour {
+
+	public HealthBarControl healthBarControl;
+
+	float currentHealth = 1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(DecreaseHealth());
+	}
+	
+	IEnumerator DecreaseHealth()
+	{
+		yield return new WaitForSeconds(1);
+		currentHealth -= 0.01f;
+
+		healthBarControl.SetBarPercent(currentHealth);
+
+		if(currentHealth >= 0)
+			StartCoroutine(DecreaseHealth());
+	}
+}
